e5dfd468b39f724e43d55aea2b1cfe095b431e7b
Fathimath Jazeera <jezijazeera@gmail.com>
2025-07-11 16:45:17 +0530

    fixed button ui

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -7,8 +7,9 @@ const Bio = ({profile}) => {
     <div className='bio-section'>
  <div className='name-section'>
  <p className='name'>{profile.name}</p>
- <p className='sub-bio'>{profile.gender| profile.age | profile.gender == "Male" ? "He/Him" : "She/Her"}</p>
+ <p className='sub-bio'>{`${profile.gender}| ${profile.age} |${profile.gender == "Male" ? "He/Him" : "She/Her"}`}</p>
  </div>
+ 
  <button className='visual-btn'>Watch my Visual Resume Now</button>
 <div className='skills-container'>
   <p>Core Skills & Technical Proficiencies</p>
